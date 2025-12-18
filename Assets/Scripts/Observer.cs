a49d4020b220c511bcbc74a007f5c674e3c582d7
KHKAC <rudgnsrb888@naver.com>
2025-12-18 09:38:17 +0900

    change

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -7,7 +7,7 @@ public class Observer : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.transform == player)
+        if (other.transform == player)
         {
             isPlayerInRange = true;
         }
@@ -15,9 +15,26 @@ public class Observer : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if(other.transform == player)
+        if (other.transform == player)
         {
             isPlayerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    Debug.Log("Player was caught!");
+                }
+            }
         }
     }
 }
