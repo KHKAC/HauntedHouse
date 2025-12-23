86c6cb7ce7852d125f5f2e0d3507f1edf8a2007b
KHKAC <rudgnsrb888@naver.com>
2025-12-23 13:21:01 +0900

    change

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -9,16 +9,26 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public UIDocument uiDoc;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+    bool hasAudioPlayed;
     bool isPlayerAtExit;
     bool isPlayerCaught;
     float timer;
     VisualElement endScreen;
     VisualElement caughtScreen;
+    float gameTimer = 0f;
+    bool gameTimerIsTicking = false;
+    Label gameTimerLabel;
 
     void Start()
     {
         endScreen = uiDoc.rootVisualElement.Q<VisualElement>("EndScreen");
         caughtScreen = uiDoc.rootVisualElement.Q<VisualElement>("CaughtScreen");
+        gameTimerLabel = uiDoc.rootVisualElement.Q<Label>("TimerLabel");
+        gameTimer = 0f;
+        gameTimerIsTicking = true;
+        UpdateTimerLabel();
     }
 
     void OnTriggerEnter(Collider other)
@@ -34,20 +44,36 @@ public class GameEnding : MonoBehaviour
         isPlayerCaught = true;    
     }
 
+    void UpdateTimerLabel()
+    {
+        gameTimerLabel.text = gameTimer.ToString("0.00");
+    }
+
     void Update()
     {
+        if (gameTimerIsTicking)
+        {
+            gameTimer += Time.deltaTime;
+            UpdateTimerLabel();
+        }
+
         if (isPlayerAtExit)
         {
-            EndLevel(endScreen, false);
+            EndLevel(endScreen, false, exitAudio);
         }
         else if (isPlayerCaught)
         {
-            EndLevel(caughtScreen, true);
+            EndLevel(caughtScreen, true, caughtAudio);
         }
     }
 
-    void EndLevel(VisualElement element, bool doRestart)
+    void EndLevel(VisualElement element, bool doRestart, AudioSource audioSource)
     {
+        if (!hasAudioPlayed)
+        {
+            audioSource.Play();
+            hasAudioPlayed = true;    
+        }
         timer += Time.deltaTime;
         element.style.opacity = timer / fadeDuration;
         if (timer > fadeDuration + displayImageDuration)
